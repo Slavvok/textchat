f34dd640123720ee315762748a12701dcafede98
Slavvok <slavvok@gmail.com>
2017-11-22 19:03:53 +0300

    Comments were added and some refactoring was made.

--- client.py
+++ client.py
@@ -4,13 +4,13 @@ def Main():
     host = '127.0.0.1'
     port = 8000
     s = socket()
-    s.connect((host,port))
-    user = input ("Input your name: ")
-    s.send(user.encode())
+    s.connect((host,port))     #Connecting to server
+    user = input ("Input your name: ")  #Username to use for chatting
+    s.send(user.encode())   #Encode converts values from str to bytes
     data = input(" -> ")
     while data != 'q':
         s.send(data.encode())
-        message = s.recv(1024).decode()
+        message = s.recv(1024).decode() #Decode converts values from bytes to str
         print (" Server answer: " + message)
         data = input(" -> ")
     s.close()
--- server.py
+++ server.py
@@ -1,42 +1,46 @@
 from socket import *
 from threading import Thread
 
-clients = []
+clients = []    #Clients list
 
 def clientHandler(c, addr):
     global clients
     user = c.recv(1024).decode()
     print('New user' + str(user) + str(addr) + 'entered chat')
-    try:
+    try: #Client session handler. Outputs exception when client leaves
         while True:
-            data = c.recv(1024).decode()
+            data = c.recv(1024).decode() #Data import
             if not data:
                 break
             print('User: ' + str(user) + ' Message: ' + str(data))
             message = '[%s] %s' % (str(user), str(data))
-            c.send(message.encode())
+            c.send(message.encode()) #Formatted data output
     except:
-        print ('%s left chat' %(user))
+        print ('%s has left the chat' %(user))
 
-HOST = '127.0.0.1'
-PORT = 8000
+def Main():
+    HOST = '127.0.0.1' #Server's host and port
+    PORT = 8000
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(10)
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(10)
 
-print('New server session: '+ str(PORT))
+    print('New server session: '+ str(PORT))
 
-trds = []
+    trds = [] #Threads list
 
-for i in range(10):
-    c, addr = s.accept()
-    clients.append(addr)
-    t = Thread(target=clientHandler, args = (c, addr))
-    trds.append(t)
-    t.start()
+    for i in range(10): #Adding number of threads depending on clients number
+        c, addr = s.accept()
+        clients.append(addr) #Adding new client
+        t = Thread(target=clientHandler, args = (c, addr))
+        trds.append(t)
+        t.start()
 
-for t in trds:
-    t.join()
+    for t in trds:
+        t.join()
 
-s.close()
+    s.close()
+
+if __name__ == '__main__':
+    Main()
